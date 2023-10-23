6c6a05996ee669d521fc19ae5fcfb0c4219077f5
advent <advent@192.168.56.1>
2023-10-23 10:03:06 +0700

    baru

---
+++ src/main/java/com/mycompany/jesen/Jesen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jesen;
+
+/**
+ *
+ * @author advent
+ */
+public class Jesen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
